8791ffc0e179e3c84c64df5780c9b4fc70a5dafb
Brindha S <brinsree@gmail.com>
2018-04-23 21:46:44 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,19 @@
 //define functions here
+function getIt(){
+$('p').on("click",function(){
+  alert("Hey!");
+});
+}
+function frameIt(){
+  $('img').on("load",function(){
+    $(this).addClass('tasty');
+  })
+}
+function pressIt(){
+  $('input').on('keydown',function(){
+    if
+  })
+}
 
 $(document).ready(function(){
 
